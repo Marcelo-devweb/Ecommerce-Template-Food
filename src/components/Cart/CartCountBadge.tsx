c878a72a3be530feab2bf3244f0413db5307d2cd
Marcelo Moreira <marcelo.desenvolvedorweb@gmail.com>
2023-09-18 10:43:21 -0300

    Ultimo Update

--- src/components/Cart/CartCountBadge.tsx
+++ src/components/Cart/CartCountBadge.tsx
@@ -5,7 +5,7 @@ interface propsType{
 
 const CartCountBadge:React.FC<propsType> = ({size}) => {
   return (
-    <div className={`absolute bg-red-600 text-white text-[14px] ${size} -right-3 -top-1 rounded-full grid place-items-center`}>3</div>
+    <div className={`absolute bg-red-600 text-white text-[14px] ${size} -right-3 -top-1 rounded-full grid place-items-center`}>0</div>
   )
 }
 export default CartCountBadge;
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,34 +4,15 @@ const Hero = () => {
   return(
     <div className="container pt-8">
       <div className="grid xl:grid-cols-3 xl:grid-rows-2 gap-8">
-        <img src="/frutas.jpg" alt="Imagem do Hero" />
-          <div className="absolute max-w-[470px] sm:ml-16 ml-8 top-[50%] -translate-y-[50%] sm:space-y-4">
-            <p className="text-2xl hidden sm:block">Frutas Secas 100% Originais</p>
-            <h2 className="text-2xl sm:text-4xl md:text-6xl font-bold">Frutas Secas de Melhor Qualidade</h2>
+        <img className="w-full h-[400px] object-cover rounded-lg" src="./banners/hero.jpg" alt="Imagem do Hero" />
+          <div className="absolute max-w-[570px] sm:ml-16 ml-8 top-[50%] -translate-y-[50%] sm:space-y-4">
+            <h2 className="text-2xl sm:text-4xl md:text-6xl font-bold text-black">Frutas Secas de Melhor Qualidade</h2>
             <p className="text-gray-500 text-xl pt-4 sm:pt-8">A partir de</p>
             <div className="font-medium text-red-600 text-2xl sm:text-4xl sm:pb-8 pb-4">R$17.50</div>
             <div className="bg-accentDark hover:bg-accent text-white rounded-full w-fit flex items-center gap-4 px-4 py-2 text-[14px] sm:px-6 sm:py-3 cursor-pointer">Compre Agora <BsArrowRight />{""}</div>
           </div>
-          <div className="relative">
-        <img className="w-full h-full object-cover rounded-lg" src="/frutas.jpg" alt="Imagem do Hero" />
-          <div className="absolute max-w-[470px] sm:ml-16 ml-8 top-[50%] -translate-y-[50%] sm:space-y-2">
-              <h2 className="text-2xl sm:text-3xl font-bold">Frutas Secas de Melhor Qualidade</h2>
-              <p className="text-gray-500 text-xl pt-4">A partir de</p>
-              <div className="font-medium text-red-600 text-2xl sm:text-4xl pb-8">R$17.50</div>
-              <div className="bg-accent hover:bg-accentDark text-white rounded-full w-fit flex items-center gap-4 px-4 py-2 text-[14px] cursor-pointer">Compre Agora <BsArrowRight />{""} </div>
-          </div>
-      </div>
-      <div className="relative">
-        <img className="w-full h-full object-cover rounded-lg" src="/back.webp" alt="Imagem do Hero" />
-          <div className="absolute max-w-[470px] sm:ml-16 ml-8 top-[50%] -translate-y-[50%] sm:space-y-2">
-              <h2 className="text-2xl sm:text-3xl font-bold">Frutas Secas de Melhor Qualidade</h2>
-              <p className="text-gray-500 text-xl pt-4">A partir de</p>
-              <div className="font-medium text-red-600 text-2xl sm:text-4xl pb-8">R$17.50</div>
-              <div className="bg-accent hover:bg-accentDark text-white rounded-full w-fit flex items-center gap-4 px-4 py-2 text-[14px] cursor-pointer">Compre Agora <BsArrowRight />{""}</div>
-          </div>
       </div>
       </div>
-    </div>
   );
 };
 
